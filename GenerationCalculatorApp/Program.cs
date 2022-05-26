91198b5969beedd9078a91a463f69e6f10d43275
Hary Prabowo Suryoatmojo <Hary.Suryoatmojo@unit4.com>
2022-05-26 10:48:59 +0700

    Add project files.

---
+++ GenerationCalculatorApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GenerationCalculatorApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
